56a70446d0e579cee3051d3689665458b9fa9191
Mirco <mirco.hinzmann@gmail.com>
2019-01-26 08:30:47 +0100

    Quad is the new Sprite

---
+++ Woodstock/Assets/Tilter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Tilter : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject pivot = new GameObject();
+        pivot.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        pivot.transform.SetParent(transform.parent);
+        transform.SetParent(pivot.transform);
+        pivot.transform.Rotate(Vector3.right, 50);
+    }
+
+}
