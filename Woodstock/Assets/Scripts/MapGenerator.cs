9efa83c294d30c7e0992f512d83b4e52ea7f5aaa
Felix Bukatz <privat@xdstudios.de>
2019-01-26 16:31:36 +0100

    Added treeArray to generation. Tree generation buggy?

--- Woodstock/Assets/Scripts/MapGenerator.cs
+++ Woodstock/Assets/Scripts/MapGenerator.cs
@@ -7,7 +7,7 @@ public class MapGenerator : MonoBehaviour
 
     public int treeDistance = 8;
 
-    public GameObject treePrefab;
+    public GameObject[] treePrefabs;
     public GameObject logPrefab;
 
     void Start()
@@ -18,6 +18,7 @@ public class MapGenerator : MonoBehaviour
 
     private void SpawnTrees()
     {
+      int treeCount = 0;
         for (int i = -5; i < 6; i++)
         {
             for (int j = -5; j < 6; j++)
@@ -26,8 +27,10 @@ public class MapGenerator : MonoBehaviour
                 var z = Random.Range(j * treeDistance, j * treeDistance + treeDistance);
                 if (IsInClearRadius(new Vector2(x, z))) continue;
 
-                var tree = Instantiate(treePrefab, transform);
+
+                var tree = Instantiate(treePrefabs[treeCount%treePrefabs.Length], transform);
                 tree.transform.position = new Vector3(x, 0, z);
+                treeCount++;
             }
         }
     }
@@ -41,11 +44,11 @@ public class MapGenerator : MonoBehaviour
             {
                 var x = Random.Range(i * logDistance, i * logDistance + logDistance);
                 var z = Random.Range(j * logDistance, j * logDistance + logDistance);
-                
+
                 if (IsInClearRadius(new Vector2(x, z))) continue;
-                
+
                 if (Random.Range(0, 2) != 1) continue;
-                
+
                 var log = Instantiate(logPrefab, transform);
                 log.transform.position = new Vector3(x, 0, z);
             }
