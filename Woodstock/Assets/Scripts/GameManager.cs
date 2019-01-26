d9f4fbd48774d750cc30734637efa9dca71d85a7
Mirco <mirco.hinzmann@gmail.com>
2019-01-26 21:22:19 +0100

    Parameter for log spawning

--- Woodstock/Assets/Scripts/GameManager.cs
+++ Woodstock/Assets/Scripts/GameManager.cs
@@ -5,10 +5,14 @@ public class GameManager : MonoBehaviour
 {
     [Tooltip("The distance to the player in that the house won't disappear when the fire burns out")]
     public int disappearDistance = 16;
-    
+
     public GameObject house;
     public GameObject character;
-    
+
+    public AudioSource gameOverSound;
+
+    private bool _playerCaught;
+
     public void FireBurntOut()
     {
         if (HouseOutOfSight())
@@ -17,11 +21,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (_playerCaught && !gameOverSound.isPlaying)
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+
     public void OnPlayerCaughtByGhost()
     {
-        SceneManager.LoadScene("End");
+        _playerCaught = true;
+        gameOverSound.Play();
     }
-    
+
     private bool HouseOutOfSight()
     {
         var distanceToHouse = (house.transform.position - character.transform.position).magnitude;
