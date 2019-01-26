7830f1cdffbe650541248b775a7c726cfc415a24
Mirco <mirco.hinzmann@gmail.com>
2019-01-26 01:42:26 +0100

    Sprites am Fuß kippen statt in der Mitte

--- Woodstock/Assets/Scripts/SpriteOrder.cs
+++ Woodstock/Assets/Scripts/SpriteOrder.cs
@@ -7,12 +7,16 @@ public class SpriteOrder : MonoBehaviour
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        GameObject pivot = new GameObject();
+        pivot.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        pivot.transform.SetParent(transform.parent);
+        transform.SetParent(pivot.transform);
+        pivot.transform.Rotate(Vector3.right, 30);
     }
 
     void LateUpdate()
     {
         _spriteRenderer.sortingOrder = (int) Camera.main.WorldToScreenPoint(_spriteRenderer.bounds.min).y * -1;
-        transform.rotation = Camera.main.transform.rotation;
-        transform.Rotate(Vector3.right, -30);
     }
 }
